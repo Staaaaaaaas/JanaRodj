9bd04fccc3dd1c519dd2fba76ea01c9c845ef5a5
Stanislav Sandler <stanislavsandler@Stanislavs-MacBook-Air.local>
2022-10-07 12:54:18 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+const express = require('express');
+const app = express();
 const http = require('http');
-
-const hostname = '104.248.227.135';
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 const port = 3000;
+const myArgs = process.argv.slice(2);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+let balls = [];
+
+
+let hostname = '104.248.227.135';
+if(myArgs[0]=="-l")hostname='127.0.0.1';
+app.use(express.static('public'));
+io.on('connection', (socket)=>{
+  balls.forEach((ball)=>{
+    socket.emit('draw ball', ball);
+  });
+  socket.on('new ball', (ball)=>{
+    balls.push(ball);
+    io.emit('draw ball', ball);
+  });
 });
+// app.get('/',(req,res)=>{
+//   res.sendFi
+// });
+
+server.listen(port, hostname);
+console.log(`Server is running at http://${hostname}:${port}`);
